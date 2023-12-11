fdbb5f50b4e1cf5ef3b6fb9c15a495733034b04d
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-11 09:46:12 -0700

    coding logical operators

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Operators;
+﻿using System;
 
-class Program
+namespace Operators
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            //var a = 1;
+            //var b = 2;
+            //var c = 3;
+
+            //Console.WriteLine(a +b * c);
+
+            //var a = 1;
+            //var b = 2;
+
+            //Console.WriteLine(!(a != b));
+
+            // LOGICAL OPERATORS
+
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            //Console.WriteLine( c > b && c == a);
+
+            Console.WriteLine(!(c > b || c == a));
+        }
     }
 }
-
